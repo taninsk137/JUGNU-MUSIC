d391fa7d6e3dcf1475a3bc8ee6456bd74d7b7f35
ponnie.tn <48283785+taninsk137@users.noreply.github.com>
2023-07-22 10:27:33 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,8 +4,8 @@ const client = require("../index");
 client.on("ready", async () => {
   console.log(`${client.user.username} Is Online`);
   client.user.setActivity({
-    name: `Made By Kabir With 💖`,
-    type: ActivityType.Watching,
+    name: `;p`,
+    type: ActivityType.Playing,
   });
 
   // loading database
