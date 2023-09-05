4fd2ab8d698d75e6e40ecbd5c733b0004f4682a4
ponnie.tn <48283785+taninsk137@users.noreply.github.com>
2023-09-05 11:53:47 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ const client = require("../index");
 client.on("ready", async () => {
   console.log(`${client.user.username} Is Online`);
   client.user.setActivity({
-    name: `;p`,
+    name: `_p`,
     type: ActivityType.Playing,
   });
 
